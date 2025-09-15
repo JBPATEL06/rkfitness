aa9d693d229a33f0202e816a10048454cd890777
Jeel Bhanderi <bhanderijeel8@gmail.com>
2025-09-15 18:08:42 +0530

    new

--- lib/Pages/homePage.dart
+++ lib/Pages/homePage.dart
@@ -12,13 +12,14 @@ class HomePage extends StatefulWidget {
 
 }
 class _HomePage extends State<HomePage>{
+  String tempImageUrl = "https://tenor.com/view/supino-gif-1051970891886466370";
   Widget build(BuildContext context){
     return Scaffold(
     appBar: PreferredSize(preferredSize: Size.fromHeight(100) , child: rkuAppBar(context)),
-        body: workout("https://imgs.search.brave.com/Zl2Mr84zSJ2SZt1x9lWKLEE4Ec3ZNGMbqxD5UqNmU7k/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9wcmV2/aWV3LnJlZGQuaXQv/d2hhdHMteW91ci1m/YXZvcml0ZS1sdWZm/eS1pbWFnZS12MC1m/YTdveHZ4YmRqaWUx/LmpwZWc_d2lkdGg9/NTM1JmF1dG89d2Vi/cCZzPTdhYTUyNGY1/Y2ZmNzI0YzcxMjI0/NTIzYmMxNDhiMzFk/ZjMxNGRlYWE",
-         "Jeel"),
+        body: workout(tempImageUrl,"Jeel"),
     );
   }
+  //App bar Created by  Jeel
   Widget rkuAppBar(BuildContext context){
     String _ProfilePic = "https://imgs.search.brave.com/Zl2Mr84zSJ2SZt1x9lWKLEE4Ec3ZNGMbqxD5UqNmU7k/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9wcmV2/aWV3LnJlZGQuaXQv/d2hhdHMteW91ci1m/YXZvcml0ZS1sdWZm/eS1pbWFnZS12MC1m/YTdveHZ4YmRqaWUx/LmpwZWc_d2lkdGg9/NTM1JmF1dG89d2Vi/cCZzPTdhYTUyNGY1/Y2ZmNzI0YzcxMjI0/NTIzYmMxNDhiMzFk/ZjMxNGRlYWE";
     return Container(
@@ -70,32 +71,39 @@ class _HomePage extends State<HomePage>{
       ),
     );
   }
-  Widget workout(String imagePath,String exerciseName){
-      return Container(
-        height: 250,
-        width: 200,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          shape:BoxShape.rectangle,
-          image: DecorationImage(
+  //Widget created by Dhruvil
+  Widget workout(String imagePath, String exerciseName) {
+    return Container(
+      height: 250,
+      width: 200,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.rectangle,
+        image: DecorationImage(
           image: NetworkImage(imagePath),
           fit: BoxFit.cover,
-          )
-          ),
-          child: Positioned(
-            top: 5,
-            right: 2,
-            bottom: 2,
-            child: Text(exerciseName, style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              backgroundColor: Colors.black45
-            ),),
-          ) ,
-          );
-                  
-      
+        ),
+      ),
+      child: Stack(
 
+        alignment: Alignment.bottomCenter,
+        children: [
+          Container(
+            width: double.infinity,
+            color: Colors.black.withOpacity(0.5),
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              exerciseName,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
