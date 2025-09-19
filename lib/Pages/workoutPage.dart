b2fa0b58eb3a6a0a9ee153ab3c341bca71b54f49
Jeel Bhanderi <bhanderijeel8@gmail.com>
2025-09-19 08:25:32 +0530

    Update workoutPage.dart

--- lib/Pages/workoutPage.dart
+++ lib/Pages/workoutPage.dart
@@ -1,15 +1,1 @@
-import 'package:flutter/cupertino.dart';
-import 'package:rkfitness/customeWidAndFun.dart';
 
-class WorkoutPage extends StatefulWidget {
-  @override
-  State<WorkoutPage> createState() => _WorkoutPage();
-
-}
-class _WorkoutPage extends State<WorkoutPage>{
-  CustomeWidAndFun mywidget = CustomeWidAndFun();
-  Widget build(BuildContext context){
-
-    return Text("hyy");
-  }
-}
