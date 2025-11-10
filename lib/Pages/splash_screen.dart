fb43841e5e607b8239bac4696320e1a3b8e90fed
Jeel Bhanderi <bhanderijeel8@gmail.com>
2025-11-10 16:49:03 +0530

    new update

--- lib/Pages/splash_screen.dart
+++ lib/Pages/splash_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:rkfitness/Pages/homePage.dart';
+import 'package:rkfitness/AdminMaster/admin_Dashboard.dart';
 import 'package:rkfitness/Pages/user_dashboard.dart';
 import 'package:rkfitness/Pages/loginpage.dart';
+import 'package:rkfitness/supabaseMaster/auth_service.dart';
+import 'package:rkfitness/supabaseMaster/useServices.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -33,19 +34,29 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
 
   Future<void> _checkAuthAndNavigate() async {
     // Add a minimum delay to show the splash screen
-    await Future.delayed(const Duration(seconds: 2));
-    
+    await Future.delayed(const Duration(seconds: 3));
+
     if (!mounted) return;
 
-    final session = Supabase.instance.client.auth.currentSession;
-    if (session != null) {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) =>  UserDashBoard()),
-      );
-    } else {
+    final authService = AuthService();
+    final session = await authService.getSession();
+
+    if (session == null) {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => const LoginPage()),
       );
+    } else {
+      final userService = UserService();
+      final user = await userService.getUser(session.user.email!);
+      if (user?.userType == 'admin') {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const AdminDashboard()),
+        );
+      } else {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const UserDashBoard()),
+        );
+      }
     }
   }
 
